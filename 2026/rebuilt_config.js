163cefbddf1edba4db6b8ec0fd2ca5b9c1e83261
Parth Suthar <parth.suthar@dynatrace.com>
2026-04-17 09:06:48 -0400

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026onbar",
+      "defaultValue": "2026oncmp2",
       "required": "true"
     },
     { "name": "Match Level",
