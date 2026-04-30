f01696c81693a4c0145446964c489332ae3bcad0
Parth Suthar <parth.suthar@dynatrace.com>
2026-04-30 06:44:51 -0500

    Update rebuilt_config.js to hopper

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026oncmp2",
+      "defaultValue": "2026hop",
       "required": "true"
     },
     { "name": "Match Level",
